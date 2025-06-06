40cffe7e0e986c7378c69e6296ab7100617e8b55
Kim Zierahn <kim@kappa-mm.de>
2025-06-06 13:05:34 +0200

    Running GPT 4.1 few shot prompt

--- exp/10_01_GPT_LLM_Zero_Shot.py
+++ exp/10_01_GPT_LLM_Zero_Shot.py
@@ -247,55 +247,56 @@ client = OpenAI(
 #
 # # save the array to a csv file
 # save_prompt_to_csv(y_pred_class_def_GPT, "class_definitions_prompt")
-
-
-
-#### Profiled simple prompt ####
-
-y_pred_profiled_simple_GPT = []
-
-# measure time in seconds
-start = time.time()
-
-# iterate over the test set and save the response for each prompt in an array
-for prompt in tqdm(X_test_profiled_simple_prompt, desc = "Profiled simple prompting"):
-    response = GPT_create_response(prompt, profiled_simple_instruction)
-    y_pred_profiled_simple_GPT.append(response)
-    # print(response)
-
-    if len(y_pred_profiled_simple_GPT) % 50 == 0 and len(y_pred_profiled_simple_GPT) > 0:
-        print(f"\n\nProcessed {len(y_pred_profiled_simple_GPT)} prompts.\n")
-        save_prompt_to_csv(y_pred_profiled_simple_GPT, "profiled_simple_prompt")
-
-end = time.time()
-calc_time(start, end, "profiled_simple_prompt")
-
-# save the array to a csv file
-save_prompt_to_csv(y_pred_profiled_simple_GPT, "profiled_simple_prompt")
-
-
-# #### Few shot prompt ####
 #
-# y_pred_few_shot_GPT = []
+#
+#
+# #### Profiled simple prompt ####
+#
+# y_pred_profiled_simple_GPT = []
 #
 # # measure time in seconds
 # start = time.time()
 #
 # # iterate over the test set and save the response for each prompt in an array
-# for prompt in tqdm(X_test_few_shot_prompt, desc = "Few-shot prompting"):
-#     response = GPT_create_response(prompt, few_shot_instruction)
-#     y_pred_few_shot_GPT.append(response)
+# for prompt in tqdm(X_test_profiled_simple_prompt, desc = "Profiled simple prompting"):
+#     response = GPT_create_response(prompt, profiled_simple_instruction)
+#     y_pred_profiled_simple_GPT.append(response)
 #     # print(response)
 #
-#     if len(y_pred_few_shot_GPT) % 50 == 0 and len(y_pred_few_shot_GPT) > 0:
-#         print(f"\n\nProcessed {len(y_pred_few_shot_GPT)} prompts.\n")
-#         save_prompt_to_csv(y_pred_few_shot_GPT, "few_shot_prompt")
+#     if len(y_pred_profiled_simple_GPT) % 50 == 0 and len(y_pred_profiled_simple_GPT) > 0:
+#         print(f"\n\nProcessed {len(y_pred_profiled_simple_GPT)} prompts.\n")
+#         save_prompt_to_csv(y_pred_profiled_simple_GPT, "profiled_simple_prompt")
 #
 # end = time.time()
-# calc_time(start, end, "few_shot_prompt")
+# calc_time(start, end, "profiled_simple_prompt")
 #
 # # save the array to a csv file
-# save_prompt_to_csv(y_pred_few_shot_GPT, "few_shot_prompt")
+# save_prompt_to_csv(y_pred_profiled_simple_GPT, "profiled_simple_prompt")
+#
+
+#### Few shot prompt ####
+
+y_pred_few_shot_GPT = []
+
+# measure time in seconds
+start = time.time()
+
+# iterate over the test set and save the response for each prompt in an array
+for prompt in tqdm(X_test_few_shot_prompt[150:], desc = "Few-shot prompting"):
+    response = GPT_create_response(prompt, few_shot_instruction)
+    y_pred_few_shot_GPT.append(response)
+    # print(response)
+
+    if len(y_pred_few_shot_GPT) % 50 == 0 and len(y_pred_few_shot_GPT) > 0:
+        print(f"\n\nProcessed {len(y_pred_few_shot_GPT)} prompts.\n")
+        save_prompt_to_csv(y_pred_few_shot_GPT, "few_shot_prompt_2")
+
+end = time.time()
+calc_time(start, end, "few_shot_prompt_2")
+
+# save the array to a csv file
+save_prompt_to_csv(y_pred_few_shot_GPT, "few_shot_prompt_2")
+
 #
 #
 # #### Vignette prompt ####
