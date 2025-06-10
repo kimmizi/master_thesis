ec69d5aa4a594c3b34bc0b5819ca5bbdb547eef1
Kim Zierahn <kim@kappa-mm.de>
2025-06-10 09:43:21 +0200

    Running Gemini vignette prompt

--- exp/10_02_Gemini_LLM_Zero_Shot.py
+++ exp/10_02_Gemini_LLM_Zero_Shot.py
@@ -292,60 +292,60 @@ client = genai.Client(
 #
 # # save the array to a csv file
 # save_prompt_to_csv(y_pred_profiled_simple_gemini, thinking_profiled_simple_gemini, "profiled_simple_prompt_2")
-
-
-
-#### Few shot prompt ####
-
-y_pred_few_shot_gemini = []
-thinking_few_shot_gemini = []
-
-# measure time in seconds
-start = time.time()
-
-# iterate over the test set and save the response for each prompt in an array
-for prompt in tqdm(X_test_few_shot_prompt, desc = "Few-shot prompting"):
-    response, thinking = Gemini_create_response(prompt, few_shot_instruction)
-    y_pred_few_shot_gemini.append(response)
-    thinking_few_shot_gemini.append(thinking)
-    # print(response)
-
-    if len(y_pred_few_shot_gemini) % 10 == 0 and len(y_pred_few_shot_gemini) > 0:
-        print(f"\n\nProcessed {len(y_pred_few_shot_gemini)} prompts.\n")
-        save_prompt_to_csv(y_pred_few_shot_gemini, thinking_few_shot_gemini, "few_shot_prompt")
-
-end = time.time()
-calc_time(start, end, "few_shot_prompt")
-
-# save the array to a csv file
-save_prompt_to_csv(y_pred_few_shot_gemini, thinking_few_shot_gemini, "few_shot_prompt")
-
-
 #
-# #### Vignette prompt ####
 #
-# y_pred_vignette_gemini = []
-# thinking_vignette_gemini = []
+#
+# #### Few shot prompt ####
+#
+# y_pred_few_shot_gemini = []
+# thinking_few_shot_gemini = []
 #
 # # measure time in seconds
 # start = time.time()
 #
 # # iterate over the test set and save the response for each prompt in an array
-# for prompt in tqdm(X_test_vignette_prompt, desc = "Vignette prompting"):
-#     response, thinking = Gemini_create_response(prompt, vignette_instruction)
-#     y_pred_vignette_gemini.append(response)
-#     thinking_vignette_gemini.append(thinking)
+# for prompt in tqdm(X_test_few_shot_prompt, desc = "Few-shot prompting"):
+#     response, thinking = Gemini_create_response(prompt, few_shot_instruction)
+#     y_pred_few_shot_gemini.append(response)
+#     thinking_few_shot_gemini.append(thinking)
 #     # print(response)
 #
-#     if len(y_pred_vignette_gemini) % 10 == 0 and len(y_pred_vignette_gemini) > 0:
-#         print(f"\n\nProcessed {len(y_pred_vignette_gemini)} prompts.\n")
-#         save_prompt_to_csv(y_pred_vignette_gemini, thinking_vignette_gemini, "vignette_prompt")
+#     if len(y_pred_few_shot_gemini) % 10 == 0 and len(y_pred_few_shot_gemini) > 0:
+#         print(f"\n\nProcessed {len(y_pred_few_shot_gemini)} prompts.\n")
+#         save_prompt_to_csv(y_pred_few_shot_gemini, thinking_few_shot_gemini, "few_shot_prompt")
 #
 # end = time.time()
-# calc_time(start, end, "vignette_prompt")
+# calc_time(start, end, "few_shot_prompt")
 #
 # # save the array to a csv file
-# save_prompt_to_csv(y_pred_vignette_gemini, thinking_vignette_gemini, "vignette_prompt")
+# save_prompt_to_csv(y_pred_few_shot_gemini, thinking_few_shot_gemini, "few_shot_prompt")
+#
+#
+#
+#### Vignette prompt ####
+
+y_pred_vignette_gemini = []
+thinking_vignette_gemini = []
+
+# measure time in seconds
+start = time.time()
+
+# iterate over the test set and save the response for each prompt in an array
+for prompt in tqdm(X_test_vignette_prompt[60:], desc = "Vignette prompting"):
+    response, thinking = Gemini_create_response(prompt, vignette_instruction)
+    y_pred_vignette_gemini.append(response)
+    thinking_vignette_gemini.append(thinking)
+    # print(response)
+
+    if len(y_pred_vignette_gemini) % 10 == 0 and len(y_pred_vignette_gemini) > 0:
+        print(f"\n\nProcessed {len(y_pred_vignette_gemini)} prompts.\n")
+        save_prompt_to_csv(y_pred_vignette_gemini, thinking_vignette_gemini, "vignette_prompt_2")
+
+end = time.time()
+calc_time(start, end, "vignette_prompt_2")
+
+# save the array to a csv file
+save_prompt_to_csv(y_pred_vignette_gemini, thinking_vignette_gemini, "vignette_prompt_2")
 #
 #
 #
