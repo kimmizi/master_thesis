97b9f74c97be82f9183c136b07d478892a17f545
Kim Zierahn <kim@kappa-mm.de>
2025-06-12 15:15:38 +0200

    Running GPT o3 vignette prompt

--- exp/10_01_GPT_o3_LLM_Zero_Shot.py
+++ exp/10_01_GPT_o3_LLM_Zero_Shot.py
@@ -297,32 +297,32 @@ client = OpenAI(
 #
 #
 #
-# #### Few shot prompt ####
-#
-# y_pred_few_shot_GPT = []
-# explanation_few_shot_GPT = []
-# thinking_few_shot_GPT = []
-#
-# # measure time in seconds
-# start = time.time()
-#
-# # iterate over the test set and save the response for each prompt in an array
-# for prompt in tqdm(X_test_few_shot_prompt, desc = "Few-shot prompting"):
-#     response, explanation, thinking = GPT_create_response(prompt, cot_instruction)
-#     y_pred_few_shot_GPT.append(response)
-#     explanation_few_shot_GPT.append(explanation)
-#     thinking_few_shot_GPT.append(thinking)
-#     # print(response)
-#
-#     if len(y_pred_few_shot_GPT) % 50 == 0 and len(y_pred_few_shot_GPT) > 0:
-#         print(f"\n\nProcessed {len(y_pred_few_shot_GPT)} prompts.\n")
-#         save_prompt_to_csv(y_pred_few_shot_GPT, explanation_few_shot_GPT, thinking_few_shot_GPT, "few_shot_prompt")
-#
-# end = time.time()
-# calc_time(start, end, "few_shot_prompt")
-#
-# # save the array to a csv file
-# save_prompt_to_csv(y_pred_few_shot_GPT, explanation_few_shot_GPT, thinking_few_shot_GPT, "few_shot_prompt")
+#### Few shot prompt ####
+
+y_pred_few_shot_GPT = []
+explanation_few_shot_GPT = []
+thinking_few_shot_GPT = []
+
+# measure time in seconds
+start = time.time()
+
+# iterate over the test set and save the response for each prompt in an array
+for prompt in tqdm(X_test_few_shot_prompt, desc = "Few-shot prompting"):
+    response, explanation, thinking = GPT_create_response(prompt, cot_instruction)
+    y_pred_few_shot_GPT.append(response)
+    explanation_few_shot_GPT.append(explanation)
+    thinking_few_shot_GPT.append(thinking)
+    # print(response)
+
+    if len(y_pred_few_shot_GPT) % 50 == 0 and len(y_pred_few_shot_GPT) > 0:
+        print(f"\n\nProcessed {len(y_pred_few_shot_GPT)} prompts.\n")
+        save_prompt_to_csv(y_pred_few_shot_GPT, explanation_few_shot_GPT, thinking_few_shot_GPT, "few_shot_prompt")
+
+end = time.time()
+calc_time(start, end, "few_shot_prompt")
+
+# save the array to a csv file
+save_prompt_to_csv(y_pred_few_shot_GPT, explanation_few_shot_GPT, thinking_few_shot_GPT, "few_shot_prompt")
 #
 #
 #
